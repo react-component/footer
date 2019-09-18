b225f6a47c28aab11f57af6c102ecaa8770b1bce
afc163 <afc163@gmail.com>
2019-09-18 20:26:46 +0800

    :wrench: update package.json

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,8 +1,6 @@
 export default {
-  entry: ['src/index.js'],
   cjs: 'babel',
   esm: { type: 'babel', importLibToEs: true },
-  doc: { base: '/footer' },
   preCommit: {
     eslint: true,
     prettier: true,
