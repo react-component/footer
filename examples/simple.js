0c6790ba87fe594b69e91b8c5912533b2e7be01f
afc163 <afc163@gmail.com>
2019-09-18 15:39:02 +0800

    :white_check_mark: fix eslint

--- examples/simple.js
+++ examples/simple.js
@@ -2,12 +2,10 @@ import '../assets/index.less';
 import React from 'react';
 import Footer from '../src/index';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Footer />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
